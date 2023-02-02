3bd71aff73cd7bc04a67d0c427e8ca2460974eb3
bruce-solo <52091577+bruce-solo@users.noreply.github.com>
2023-02-02 18:39:24 +0800

    update GitTest.java add hello github2

--- src/main/java/com/mockingjay/git_demo/GitTest.java
+++ src/main/java/com/mockingjay/git_demo/GitTest.java
@@ -3,5 +3,6 @@ package com.mockingjay.git_demo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello github");
+        System.out.println("hello github2");
     }
 }
