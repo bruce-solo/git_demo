f10c9b966429dd3344068d2f1e2ac8277dbbf9a1
bruce-solo <52091577+bruce-solo@users.noreply.github.com>
2023-02-28 13:20:10 +0800

    delete 237

--- src/main/java/com/mockingjay/git_demo/GitTest.java
+++ src/main/java/com/mockingjay/git_demo/GitTest.java
@@ -3,8 +3,5 @@ package com.mockingjay.git_demo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello github");
-        System.out.println("hello github2");
-        System.out.println("hello github3");
-        System.out.println("hello github7");
     }
 }
